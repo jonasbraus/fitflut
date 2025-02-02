fcbb1a649b3fe9e13b309c95788d3a64bc7b62d1
Jonas Braus <brausjonas@gmail.com>
2025-02-02 20:00:42 +0100

    Update PageSettings.dart

--- lib/pages/PageSettings.dart
+++ lib/pages/PageSettings.dart
@@ -29,141 +29,137 @@ class PageSettings extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 0),
-      child: Row(
-        children: [
-          Consumer<LanguageProvider>(
-            builder: (context, provLang, child) => Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+      child: Consumer<LanguageProvider>(
+        builder: (context, provLang, child) => Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              LanguageProvider.getMap()["settings"]["colorscheme"],
+              style: TextStyle(fontSize: 18),
+            ),
+            Consumer<SettingsProvider>(
+              builder: (context, prov, child) => Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Radio<int>(
+                    onChanged: (value) =>
+                    {prov.selectColor(0), storeColorScheme(0)},
+                    value: 0,
+                    groupValue: SettingsProvider.selectedColor,
+                    fillColor: WidgetStatePropertyAll(Colors.blue),
+                  ),
+                  Radio<int>(
+                    onChanged: (value) =>
+                    {prov.selectColor(1), storeColorScheme(1)},
+                    value: 1,
+                    groupValue: SettingsProvider.selectedColor,
+                    fillColor: WidgetStatePropertyAll(Colors.green),
+                  ),
+                  Radio<int>(
+                    onChanged: (value) =>
+                    {prov.selectColor(2), storeColorScheme(2)},
+                    value: 2,
+                    groupValue: SettingsProvider.selectedColor,
+                    fillColor: WidgetStatePropertyAll(Colors.purple),
+                  ),
+                  Radio<int>(
+                    onChanged: (value) =>
+                    {prov.selectColor(3), storeColorScheme(3)},
+                    value: 3,
+                    groupValue: SettingsProvider.selectedColor,
+                    fillColor: WidgetStatePropertyAll(Colors.red),
+                  ),
+                  Radio<int>(
+                    onChanged: (value) =>
+                    {prov.selectColor(4), storeColorScheme(4)},
+                    value: 4,
+                    groupValue: SettingsProvider.selectedColor,
+                    fillColor: WidgetStatePropertyAll(Colors.orange),
+                  )
+                ],
+              ),
+            ),
+            Text(LanguageProvider.getMap()["settings"]["language"],
+                style: TextStyle(fontSize: 18)),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  LanguageProvider.getMap()["settings"]["colorscheme"],
-                  style: TextStyle(fontSize: 18),
-                ),
-                Consumer<SettingsProvider>(
-                  builder: (context, prov, child) => Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Radio<int>(
-                        onChanged: (value) =>
-                            {prov.selectColor(0), storeColorScheme(0)},
-                        value: 0,
-                        groupValue: SettingsProvider.selectedColor,
-                        fillColor: WidgetStatePropertyAll(Colors.blue),
-                      ),
-                      Radio<int>(
-                        onChanged: (value) =>
-                            {prov.selectColor(1), storeColorScheme(1)},
-                        value: 1,
-                        groupValue: SettingsProvider.selectedColor,
-                        fillColor: WidgetStatePropertyAll(Colors.green),
-                      ),
-                      Radio<int>(
-                        onChanged: (value) =>
-                            {prov.selectColor(2), storeColorScheme(2)},
-                        value: 2,
-                        groupValue: SettingsProvider.selectedColor,
-                        fillColor: WidgetStatePropertyAll(Colors.purple),
-                      ),
-                      Radio<int>(
-                        onChanged: (value) =>
-                            {prov.selectColor(3), storeColorScheme(3)},
-                        value: 3,
-                        groupValue: SettingsProvider.selectedColor,
-                        fillColor: WidgetStatePropertyAll(Colors.red),
-                      ),
-                      Radio<int>(
-                        onChanged: (value) =>
-                            {prov.selectColor(4), storeColorScheme(4)},
-                        value: 4,
-                        groupValue: SettingsProvider.selectedColor,
-                        fillColor: WidgetStatePropertyAll(Colors.orange),
-                      )
-                    ],
+                FilledButton(
+                  onPressed: () =>
+                  {provLang.updateLang("en"), storeLanguage("en")},
+                  child: Text("en"),
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(
+                        LanguageProvider.lang == "en"
+                            ? Theme.of(context)
+                            .colorScheme
+                            .primary
+                            .withAlpha(100)
+                            : Theme.of(context).colorScheme.primary),
                   ),
                 ),
-                Text(LanguageProvider.getMap()["settings"]["language"],
-                    style: TextStyle(fontSize: 18)),
                 SizedBox(
-                  height: 10,
+                  width: 10,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FilledButton(
-                      onPressed: () =>
-                          {provLang.updateLang("en"), storeLanguage("en")},
-                      child: Text("en"),
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStatePropertyAll(
-                            LanguageProvider.lang == "en"
-                                ? Theme.of(context)
-                                    .colorScheme
-                                    .primary
-                                    .withAlpha(100)
-                                : Theme.of(context).colorScheme.primary),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    FilledButton(
-                      onPressed: () => {
-                        provLang.updateLang("de"),
-                        storeLanguage("de"),
-                      },
-                      child: Text("de"),
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStatePropertyAll(
-                            LanguageProvider.lang == "de"
-                                ? Theme.of(context)
-                                    .colorScheme
-                                    .primary
-                                    .withAlpha(100)
-                                : Theme.of(context).colorScheme.primary),
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(LanguageProvider.getMap()["settings"]["gymgoal"],
-                    style: TextStyle(fontSize: 17)),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Consumer<GymgoalProvider>(
-                      builder: (context, goalProv, child) => Row(
-                        children: [
-                          Slider(
-                            value: GymgoalProvider.goal.toDouble(),
-                            onChanged: (value) => {
-                              goalProv.setGoal(
-                                value.toInt(),
-                              ),
-                              storeGymgoal(value.toInt())
-                            },
-                            divisions: 7,
-                            min: 0,
-                            max: 7,
-                          ),
-                          SizedBox(width: 10),
-                          Text(
-                            GymgoalProvider.goal.toString(),
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
+                FilledButton(
+                  onPressed: () => {
+                    provLang.updateLang("de"),
+                    storeLanguage("de"),
+                  },
+                  child: Text("de"),
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(
+                        LanguageProvider.lang == "de"
+                            ? Theme.of(context)
+                            .colorScheme
+                            .primary
+                            .withAlpha(100)
+                            : Theme.of(context).colorScheme.primary),
+                  ),
                 )
               ],
             ),
-          )
-        ],
-      ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(LanguageProvider.getMap()["settings"]["gymgoal"],
+                style: TextStyle(fontSize: 17)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Consumer<GymgoalProvider>(
+                  builder: (context, goalProv, child) => Row(
+                    children: [
+                      Slider(
+                        value: GymgoalProvider.goal.toDouble(),
+                        onChanged: (value) => {
+                          goalProv.setGoal(
+                            value.toInt(),
+                          ),
+                          storeGymgoal(value.toInt())
+                        },
+                        divisions: 7,
+                        min: 0,
+                        max: 7,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        GymgoalProvider.goal.toString(),
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
