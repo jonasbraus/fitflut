09847d412620d081bcc2e9d00c41fe08fedb018a
Jonas Braus <brausjonas@gmail.com>
2025-02-05 22:50:48 +0100

    nice login

--- lib/pages/PageSettings.dart
+++ lib/pages/PageSettings.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:fitflut/pages/PageAccount.dart';
 import 'package:fitflut/providers/GymgoalProvider.dart';
 import 'package:fitflut/providers/LoginProvider.dart';
 import 'package:fitflut/workoutPages/SettingsProvider.dart';
@@ -31,17 +32,14 @@ class PageSettings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String email = "";
-    String password = "";
-
     return Padding(
         padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 0),
         child: Consumer<LanguageProvider>(
           builder: (context, provLang, child) => Column(
             mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text(LanguageProvider.getMap()["settings"]["language"],
+              Text(LanguageProvider.getMap()["settings"]["language"] + " | Account",
                   style: TextStyle(fontSize: 18)),
               SizedBox(
                 height: 10,
@@ -84,6 +82,18 @@ class PageSettings extends StatelessWidget {
                   )
                 ],
               ),
+              SizedBox(height: 30,),
+              FilledButton.icon(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => PageAccount(),));
+                },
+                label: Text("Account"),
+                icon: Icon(Icons.person),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(),
               SizedBox(
                 height: 10,
               ),
@@ -119,122 +129,13 @@ class PageSettings extends StatelessWidget {
                 ],
               ),
               SizedBox(
-                height: 20,
+                height: 10,
               ),
-              Text(
-                "Account",
-                style: TextStyle(fontSize: 18),
-              ),
+              Divider(),
               SizedBox(
                 height: 10,
               ),
-              Consumer<LoginProvider>(
-                builder: (context, loginProv, child) {
-                  if (LoginProvider.user == null) {
-                    return Column(
-                      children: [
-                        TextField(
-                          decoration: InputDecoration(
-                            labelText: "Email",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                          ),
-                          onChanged: (value) => email = value,
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextField(
-                          decoration: InputDecoration(
-                            labelText: "Password",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                          ),
-                          obscureText: true,
-                          onChanged: (value) => password = value,
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        FilledButton.icon(
-                          onPressed: () async {
-                            print(LoginProvider.user);
-                            try {
-                              print("!!!!!!!!!!!!!!!!!!!!!!!!!");
-                              print("!!!!!!!!!!!!!!!!!!!!!!!!!");
-                              print("!!!!!!!!!!!!!!!!!!!!!!!!!");
-                              print("!!!!!!!!!!!!!!!!!!!!!!!!!");
-                              print("!!!!!!!!!!!!!!!!!!!!!!!!!");
-                              print(email);
-                              print(password);
-                              await FirebaseAuth.instance
-                                  .signInWithEmailAndPassword(
-                                      email: email, password: password);
-                              loginProv
-                                  .login(FirebaseAuth.instance.currentUser!);
-                            } on FirebaseAuthException catch (e) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                      content:
-                                          Text("Wrong Email or Password!")));
-                            }
-                          },
-                          label: Text("Login"),
-                          icon: Icon(Icons.person),
-                        )
-                      ],
-                    );
-                  }
 
-                  return FilledButton.icon(
-                    onPressed: () async {
-                      showDialog(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                          contentPadding: EdgeInsets.only(top: 18, left: 18),
-                          actionsPadding: EdgeInsets.all(10),
-                          actionsAlignment: MainAxisAlignment.spaceBetween,
-                          insetPadding: EdgeInsets.all(0),
-                          content: Text(
-                            LanguageProvider.getMap()["general"]["sure"],
-                            style: TextStyle(fontSize: 18),
-                          ),
-                          actions: [
-                            TextButton.icon(
-                              onPressed: () async => {
-                                await FirebaseAuth.instance.signOut(),
-                                loginProv.logout(),
-                                Navigator.of(context).pop()
-                              },
-                              label: Text("Logout",
-                                style: TextStyle(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .onSurface),
-                              ),
-                              icon: Icon(
-                                Icons.person,
-                                color:
-                                Theme.of(context).colorScheme.onSurface,
-                              ),
-                            ),
-                            TextButton.icon(
-                              onPressed: () => {Navigator.of(context).pop()},
-                              label: Text(LanguageProvider.getMap()["general"]
-                              ["cancel"]),
-                              icon: Icon(Icons.arrow_back),
-                            )
-                          ],
-                        ),
-                      );
-                    },
-                    label: Text("Logout"),
-                    icon: Icon(Icons.person),
-                  );
-                },
-              ),
             ],
           ),
         ));
